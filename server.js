c71fdbbab0314b11a5389436bbb8f78f6b10e98e
joldie <j.oldridge@mykolab.com>
2018-12-20 15:52:58 +0100

    Functionality complete.

--- server.js
+++ server.js
@@ -7,16 +7,25 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const urlExists = require("url-exists");
+const shortid = require("shortid");
 
-// Express.js server
+// Save site URL
+const projectUrl = "http://localhost:8080";
+
+// Express.js server, with CORS enabled
 const app = express();
-
-// Enable CORS
 app.use(cors());
 
-// Connect to Mongo database and setup schema
-mongoose.connect(process.env.MONGO_URI);
+// Mount body-parser for parsing POST bodies
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Connect to Mongo database
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true }
+);
+
+// Setup database schema
 const urlSchema = new mongoose.Schema({
   original_url: { type: String, required: true },
   short_url_id: String,
@@ -24,23 +33,62 @@ const urlSchema = new mongoose.Schema({
 });
 const ShortURL = mongoose.model("ShortURL", urlSchema);
 
-// Mount body-parser for parsing POST bodies
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // Default landing page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// API endpoint
+// API endpoint - POST new URL to shorten
 app.post("/api/shorturl/new", (req, res) => {
   urlExists(req.body.url, function(err, exists) {
-    if (!exists || err) {
+    if (err) return res.send(err);
+    if (!exists) {
       res.json({ error: "invalid URL" });
     } else {
-      res.json({ no_error: "valid URL" });
+      ShortURL.findOne({ original_url: req.body.url }, (err, data) => {
+        if (err) return res.send(err);
+        if (data) {
+          res.json({
+            original_url: data.original_url,
+            short_url_id: data.short_url_id,
+            short_url_full: data.short_url_full
+          });
+        } else {
+          const short_url = shortid.generate();
+          const newURL = new ShortURL({
+            original_url: req.body.url,
+            short_url_id: short_url,
+            short_url_full: projectUrl + "/api/shorturl/" + short_url
+          })
+            .save()
+            .then(data =>
+              res.json({
+                original_url: data.original_url,
+                short_url_id: data.short_url_id,
+                short_url_full: data.short_url_full
+              })
+            )
+            .catch(err => res.send(err));
+        }
+      });
     }
   });
+});
+
+// API endpoint - GET original URL from shortened one
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  if (req.params.shorturl === "new") {
+    res.redirect(projectUrl);
+  } else {
+    ShortURL.findOne({ short_url_id: req.params.shorturl }, (err, data) => {
+      if (err) return res.send(err);
+      if (data) {
+        res.redirect(data.original_url);
+      } else {
+        res.json({ error: "URL not found in database" });
+      }
+    });
+  }
 });
 
 // Listen for requests
